1b723a4bd065b8254a4067fcbfef2586dc64d550
Vikranth3140 <vikranth22570@iiitd.ac.in>
2024-10-06 11:05:50 +0530

    2nd try

--- 1.py
+++ 1.py
@@ -7,21 +7,25 @@ def calculateTotalRegion(heights):
     
     # Calculate left array
     for i in range(n):
-        if i == 0 or heights[i] > heights[i - 1]:
+        if i == 0:
             left[i] = 1
-        else:
+        elif heights[i] >= heights[i - 1]:
             left[i] = left[i - 1] + 1
+        else:
+            left[i] = 1
     
     # Calculate right array
     for i in range(n - 1, -1, -1):
-        if i == n - 1 or heights[i] > heights[i + 1]:
+        if i == n - 1:
             right[i] = 1
-        else:
+        elif heights[i] >= heights[i + 1]:
             right[i] = right[i + 1] + 1
+        else:
+            right[i] = 1
     
     # Calculate the total sum of all regions
     total_sum = 0
     for i in range(n):
-        total_sum += left[i] + right[i] - 1
+        total_sum += left[i] + right[i] - 1  # Subtract 1 to avoid double counting
     
     return total_sum
